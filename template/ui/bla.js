a610fe07cc84c26a7d141843ff69b0bd41e1ce3d
Nguyễn Quốc Anh Tuấn <147523268+tuan0919@users.noreply.github.com>
2024-07-18 20:31:07 +0700

    back-up

---
+++ template/ui/bla.js
@@ -0,0 +1,77 @@
+const a = {
+    draw: 3,
+    columns: [
+      {
+        data: "id",
+        name: "",
+        searchable: true,
+        orderable: true,
+        search: {
+          value: "",
+          regex: false
+        }
+      },
+      {
+        data: null,
+        name: "",
+        searchable: true,
+        orderable: true,
+        search: {
+          value: "",
+          regex: false
+        }
+      },
+      {
+        data: "changeValue",
+        name: "",
+        searchable: true,
+        orderable: true,
+        search: {
+          value: "",
+          regex: false
+        }
+      },
+      {
+        data: "type",
+        name: "",
+        searchable: true,
+        orderable: true,
+        search: {
+          value: "",
+          regex: false
+        }
+      },
+      {
+        data: "createAt",
+        name: "",
+        searchable: true,
+        orderable: true,
+        search: {
+          value: "THEM_SKU_MOI",
+          regex: false
+        }
+      },
+      {
+        data: "note",
+        name: "",
+        searchable: true,
+        orderable: true,
+        search: {
+          value: "",
+          regex: false
+        }
+      }
+    ],
+    order: [
+      {
+        column: 0,
+        dir: "asc"
+      }
+    ],
+    start: 0,
+    length: 10,
+    search: {
+      value: "",
+      regex: false
+    }
+  }
